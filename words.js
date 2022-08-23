4d45c3faf4f052e72e7c03e78937bbf091eaf7e4
Miriam Poletti <polettimiriam@gmail.com>
2022-08-23 10:08:08 +0200

    changed structure

---
+++ words.js
@@ -0,0 +1,49 @@
+const wordList = [
+    {
+        word: "unemployment"
+    },
+    {
+        word: "depression"
+    },
+    {
+        word: "student debt"
+    },
+    {
+        word: "unpaid internships"
+    },
+    {
+        word: "deleuze quotes"
+    },
+    {
+        word: "failure"
+    },
+    {
+        word: "exploitation"
+    },
+    {
+        word: "memes"
+    },
+    {
+        word: "unrelated jobs"
+    },
+    {
+        word: "alcoholism"
+    },
+    {
+        word: "unsold NFTs"
+    },
+    {
+        word: "precarity"
+    },
+    {
+        word: "mental health issues"
+    },
+    {
+        word: "overwork"
+    },
+    {
+        word: "rejections"
+    },
+    {
+        word: "instagram takeovers"
+    }]
